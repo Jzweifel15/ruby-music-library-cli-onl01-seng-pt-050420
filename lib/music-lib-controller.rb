c8d33e529ba1d6e4bc556ac5490fe2f1623cab74
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-06-15 19:22:08 +0000

    Automatically backed up by Learn

---
+++ lib/music-lib-controller.rb
@@ -0,0 +1,75 @@
+class MusicLibraryController
+  
+    attr_reader :path
+    
+    def initialize(path = './db/mp3s') 
+      @path = path
+      MusicImporter.new(@path).import
+    end
+    
+    def call 
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      while true
+        input = gets.strip
+        case input
+        when "exit"
+          break;
+        when "list songs"
+          list_songs
+        when "list artists"
+          list_artists
+        when "list genres"
+          list_genres
+        when "play song"
+          play_song
+        when "list artist"
+          list_artist
+        when "list genre"
+          list_genre
+        else
+          puts "Unknown command.  Please try again, or 'exit' to quit."
+        end
+      end
+  
+    end
+    
+    def list_songs
+      sorted_songs = Song.all.sort_by {|song|song.name}
+      sorted_songs.each {|song|
+        puts "#{sorted_library.index(song) + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+    end
+    
+    def list_artists
+      sorted_artists = Artists.all.sort
+      sorted_artists.each_with_index {|artist, index| puts "#{index + 1}. #{artist.name}"}
+    end
+    
+    def list_genres
+      Genre.all.each {|genre| puts "#{genre.name}"}
+    end
+    
+    def list_songs_by_artist
+    end
+    
+    def list_songs_by_genre
+    end
+    
+    def play_song
+      puts "Enter the index of the song you would like to play =>"
+      input = gets.strip
+  
+      if song_index = input.to_i
+        song = Song.all[song_index - 1]
+        puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      end
+    end
+    
+  end
