e4298fbf74343344e3efc6028bad15cf79c8e266
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-06-15 19:54:18 +0000

    Automatically backed up by Learn

--- lib/music-lib-controller.rb
+++ lib/music-lib-controller.rb
@@ -63,7 +63,7 @@ class MusicLibraryController
       puts "Please enter the name of an artist:"
       input = gets.strip
       if artist = Artist.find_by_name(input)
-        artist.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |song, index|
+        artist.songs.sort {|a, b| a.name <=> b.name}.each_with_index do |song, index|
           puts "#{index + 1}. #{song.name} - #{song.genre.name}"
         end
       end
@@ -73,7 +73,9 @@ class MusicLibraryController
       puts "Please enter the name of a genre:"
       input = gets.strip
       if genre = Genre.find_by_name(input)
-        
+        genre.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |song, index|
+          puts "#{index + 1}. #{song.name"
+        end
       end
     end
     
